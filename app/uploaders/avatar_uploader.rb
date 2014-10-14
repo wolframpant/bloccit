48461969db0a7d52ded8ea6a2bca8e9565bca2dd
Ellen W <llnwlfsn@gmail.com>
2014-10-14 22:05:26 +0000

    Avatar Checkpoint

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -24,7 +24,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+process resize_to_fill: [90, 90]
   #
   # def scale(width, height)
   #   # do something
