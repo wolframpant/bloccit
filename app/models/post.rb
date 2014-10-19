e7527333165bbd5eb191bf1c71d11728eafea8fc
Ellen W <llnwlfsn@gmail.com>
2014-10-19 02:36:16 +0000

    Voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,40 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
   belongs_to :topic
+  has_many :votes, dependent: :destroy
 
   mount_uploader :image, ImageUploader
 
-  default_scope {order('created_at DESC')}
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  default_scope {order('rank DESC')}
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+  # validates :topic, presence: true
+  # validates :user, presence: true
+  def update_rank
+    age = (created_at - Time.new(1970,1,1)) / (60*60*24)
+    new_rank = points + age
 
+    update_attribute(:rank, new_rank)
+  end
+
+  after_create :create_vote
+
+  private
+
+  def create_vote
+    @vote = user.votes.create(value: 1, post: self)
+  end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,15 +1,15 @@
-describe Vote do
-  before do
-  v = Vote.new(value: 1)
-  v2 = Vote.new(value: -1)
-  v3 = Vote.new(value: 2)
-  end
+require 'rails_helper'
 
+describe Vote do
+ 
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect(v || v2).to eq(true)
-        expect(v3).to eq(false)
+        v = Vote.new(value: 1)
+        v2 = Vote.new(value: -1)
+        v3 = Vote.new(value: 2)
+        expect(v.valid? || v2.valid?).to eq(true)
+        expect(v3.valid?).to eq(false)
       end
     end
   end
