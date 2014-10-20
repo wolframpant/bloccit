7bcbd722fe55967c7c2a69bdd49bf947d05f3502
Ellen W <llnwlfsn@gmail.com>
2014-10-20 23:45:25 +0000

    specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,19 @@
 
   include TestFactories
 
+  before do
+    @post = associated_post
+    @user = authenticated_user
+  end
+
   describe "#favorited(post)" do
-    xit "returns 'nil' if the user has not favorited the post" do
+    it "returns 'nil' if the user has not favorited the post" do
+      expect( @user.favorites.find_by_post_id(@post.id) ).to eq(nil)
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.create(post: @post)
+      expect( @user.favorites.find_by_post_id(@post.id) ).to eq(favorite)
     end
   end
 end
