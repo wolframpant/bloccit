e7b7934f030b8409381ed7da80e1336847036a22
Ellen W <llnwlfsn@gmail.com>
2014-10-21 23:17:27 +0000

    including Warden

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe "Visiting profiles" do
 
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "A comment")
+    @comment = Comment.new(post: @post, user: @user, body: "A comment")
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save
   end
@@ -24,4 +26,24 @@ describe "Visiting profiles" do
     end
 
   end
+
+  describe "signed in" do
+
+    before do
+      login_as(@user, scope: :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+
+      Warden.test_reset!
+    end
+
+end
+
 end
